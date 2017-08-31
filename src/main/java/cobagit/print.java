9cdbbde565bac821d299fa2ad76c783b3527a7cd
Aditya- <aditya@indonesiaindicator.com>
2017-08-31 15:13:57 +0700

    print.java ditambah 1 baris kata

--- src/main/java/cobagit/print.java
+++ src/main/java/cobagit/print.java
@@ -5,6 +5,8 @@ public class print {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		
+		System.out.println("coba push aja");
 	}
 
 }
